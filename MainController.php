914599c7c2a50c8f1439189214184a2a0f402fc8
Filipp Lepalaan <filipp@mac.com>
2010-10-16 18:15:20 +0300

    Added license

--- MainController.php
+++ MainController.php
@@ -2,6 +2,13 @@
 ////
 // main/MainController.php
 // @TODO: transfer boeuf.php here
+
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */
+ 
 class MainController
 {
   public $view;                   // Where to store the data to be rendered
--- MainView.php
+++ MainView.php
@@ -3,6 +3,13 @@
 // main/MainView.php
 // @created 31.10.2009
 // @author Filipp Lepalaan <filipp@mac.com>
+
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */
+ 
 class MainView
 {
   ////
